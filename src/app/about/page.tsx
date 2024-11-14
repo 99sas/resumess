eefff1a75e9773eec08d231482a5c6dd4ab13a0d
99sas <164977732+99sas@users.noreply.github.com>
2024-11-14 12:23:10 +0500

    changes

---
+++ src/app/about/page.tsx
@@ -0,0 +1,17 @@
+import About from "@/components/about"
+import Education from "@/components/education"
+
+
+
+
+
+
+function AboutPage() {
+  return(
+    <main>
+        
+        <About/>
+        <Education/>
+    </main>
+  )
+}export default AboutPage
---
+++ src/app/contect/page.tsx
@@ -0,0 +1,12 @@
+import Contect from "@/components/contect";
+
+
+function Contentpage() {
+    return (
+        <main>
+            <Contect/> {/* Render the imported Contect component here */}
+        </main>
+    );
+}
+
+export default Contentpage;
